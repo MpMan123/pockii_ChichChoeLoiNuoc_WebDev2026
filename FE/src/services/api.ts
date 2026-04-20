0d46f66f20a17419bf7ffefe7187c0380e3a250b
MpMan123 <mhp230306@gmail.com>
2026-04-20 13:00:40 +0700

    chac chay duoc v5

--- FE/src/services/api.ts
+++ FE/src/services/api.ts
@@ -10,10 +10,12 @@ export const api = axios.create({
     withCredentials: true
 });
 
+api.interceptors
+
 api.interceptors.response.use(
     (response) => response,
     (error) => {
-        if (error.response.status === 401) {
+        if (error.response?.status === 401) {
             localStorage.removeItem('token');
             localStorage.removeItem('user');
             window.location.href = '/login';
