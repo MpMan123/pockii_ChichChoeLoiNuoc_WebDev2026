ebb48d664f582e5d4bf03d36b1c25618bd5e52f5
MpMan123 <mhp230306@gmail.com>
2026-04-20 14:23:47 +0700

    chay di ma v8

--- BE/controller/auth.controller.js
+++ BE/controller/auth.controller.js
@@ -27,7 +27,7 @@ export const login = async (req, res) => {
         res.cookie('access_token', data.session.access_token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
